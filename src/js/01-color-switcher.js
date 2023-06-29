d7762a5cecbb0dc323109eed8a4f73b5fc3e1222
ewasawczynska <sawczynska.e@gmail.com>
2023-06-29 20:08:25 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+}
+
+const startClick = () => {
+  timerId = setInterval(changeBackgroundColor, 1000);
+  buttonStart.setAttribute(`disabled`, '');
+  buttonStop.removeAttribute(`disabled`);
+};
+
+const stopClick = () => {
+  clearInterval(timerId);
+  buttonStop.setAttribute(`disabled`, '');
+  buttonStart.removeAttribute(`disabled`);
+};
+
+buttonStart.addEventListener('click', startClick);
+buttonStop.addEventListener('click', stopClick);
