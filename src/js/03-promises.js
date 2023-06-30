d86ef61b9e08e2a866546312fdb740cd015c5e6c
ewasawczynska <sawczynska.e@gmail.com>
2023-06-30 12:00:22 +0200

    task 3 - in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,27 @@
+import Notiflix from 'notiflix';
+
+const button = document.querySelector('button');
+const form = document.querySelector('form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      const result = {
+        position: position,
+        delay: delay,
+      };
+      if (shouldResolve) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    }, delay);
+  });
 }
+
+function submit(event) {
+  event.preventDefault();
+}
+
+button.addEventListener('click', submit);
