a26e9e531dab2802efadc620af61fe479e909496
ewasawczynska <sawczynska.e@gmail.com>
2023-07-03 22:06:18 +0200

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -22,6 +22,22 @@ function createPromise(position, delay) {
 
 function submit(event) {
   event.preventDefault();
+  const firstDelay = Number(form.delay.value);
+  const delayStep = Number(form.step.value);
+  const amount = Number(form.amount.value);
+  for (let index = 0; index < amount; index++) {
+    createPromise(index + 1, firstDelay + delayStep * index)
+      .then(result => {
+        Notiflix.Notify.success(
+          `Fulfilled promise ${result.position} in ${result.delay} ms`
+        );
+      })
+      .catch(result => {
+        Notiflix.Notify.failure(
+          `Rejected promise ${result.position} in ${result.delay} ms`
+        );
+      });
+  }
 }
 
 button.addEventListener('click', submit);
